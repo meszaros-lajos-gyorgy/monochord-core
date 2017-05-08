2c6f8a40d23998f5f2e7b47144cf7036d868ce18
Lajos.Meszaros <m_lajos@hotmail.com>
2017-05-08 23:05:12 +0200

    chore(package): Replaced husky with pre-commit; removed version from build script
    Replaced husky, since the precommit script was being triggered by npm run commit. Version is no
    longer in the package.json file, so I will have to get that from somewhere else

--- rollem.config.js
+++ rollem.config.js
@@ -1,21 +1,9 @@
-import fs from 'fs'
 import uglify from 'rollup-plugin-uglify'
 import buble from 'rollup-plugin-buble'
 
-const getVersionPostfix = () => {
-  let version = ''
-
-  try {
-    const config = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
-    version = config.version
-  } catch (e) {}
-
-  return (version ? '-' + version : '')
-}
-
 export default [{
   entry: 'src/index.js',
-  dest: 'dist/monochord-core' + getVersionPostfix() + '.js',
+  dest: 'dist/monochord-core.js',
   format: 'umd',
   moduleName: 'MonochordCore',
   plugins: [
@@ -23,7 +11,7 @@ export default [{
   ]
 }, {
   entry: 'src/index.js',
-  dest: 'dist/monochord-core' + getVersionPostfix() + '.min.js',
+  dest: 'dist/monochord-core.min.js',
   format: 'umd',
   moduleName: 'MonochordCore',
   sourceMap: true,
