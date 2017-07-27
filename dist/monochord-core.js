d14f6c3f55f4b4efb710c773ba8b9fb4287267b7
Lajos Meszaros <lajos.meszaros@performgroup.com>
2017-07-27 16:41:10 +0200

    chore(gitignore): Removed reify-cache and added package-lock to gitignore
    We no longer reify, so no need to have it in the ignored folder list

--- dist/monochord-core.js
+++ dist/monochord-core.js
@@ -1,4 +1,4 @@
-// monochord-core - created by Lajos Meszaros <m_lajos@hotmail.com> - MIT licence - last built on 2017-06-28
+// monochord-core - created by Lajos Meszaros <m_lajos@hotmail.com> - MIT licence - last built on 2017-07-27
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
