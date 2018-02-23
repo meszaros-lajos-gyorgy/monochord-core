306ccad3e4e9370a920dec72913dc6789acc70a2
Lajos.Meszaros <m_lajos@hotmail.com>
2018-02-23 18:00:08 +0100

    refactor(math): Moved contents of math/index to math/basic
    Also: created math/advanced, which will hold math functions of convert, like prime factorization

---
+++ src/math/advanced.js
@@ -0,0 +1,32 @@
+/*
+import {
+  wrapBinary,
+  wrapUnary,
+  number,
+  invert
+} from './helpers'
+
+import {
+  Errors
+} from './constants'
+
+import {
+  Either
+} from 'ramda-fantasy'
+
+import {
+  always,
+  tryCatch,
+  converge,
+  nthArg,
+  compose
+} from 'ramda'
+*/
+
+// -----------------
+
+// -----------------
+
+export {
+
+}
--- test/math/basic.spec.js
+++ test/math/basic.spec.js
@@ -8,7 +8,7 @@ import {
 
 import {
   number,
-
+  
   add,
   subtract,
   multiply,
